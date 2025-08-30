dfef50e1552e29fe16c801f550d89430407d9bd9
AFSAecom <afsaecom@gmail.com>
2025-08-30 15:52:50 +0100

    refactor moto page to read supabase directly

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'paiswydlxugupnlalryu.supabase.co',
-        pathname: '/storage/v1/object/public/**',
-      },
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
     ],
   },
 };
-
 module.exports = nextConfig;
